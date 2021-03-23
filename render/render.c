cec32c56de5dfd415dcf2384f8b514811a629a48
Valentin Beaufays <vbeaufay@student.s19.be>
2021-03-23 17:42:43 +0100

    save

--- render/render.c
+++ render/render.c
@@ -27,13 +27,17 @@ typedef struct	mlx
 
 typedef struct  s_ray
 {
-	double		hX;
-	double		hY;
-	double		vX;
-	double		vY;
+	int			up = 0;
+	int			left = 0;
+	double		hX = -1;
+	double		hY = -1;
+	double		vX = -1;
+	double		vY = -1;
+	double		x = -1; 
+	double		y = -1; 
 	double		stepX;
 	double		stepY;
-	double		rayAngle;
+	double		rayAngle = mlx->angle + (mlx->fov / 2); //check if > 2 * M_PI;
 }               t_ray;
 
 int Map[mapWidth][mapHeight]=
@@ -56,77 +60,139 @@ void getRayDir(double angle, int *up, int *left)
 		*left = -1;
 }
 
+int	check_hit(t_ray *ray/*, char **map*/)
+{
+	if (ray->x >= 0 && ray->y >= 0 && ray->x < mapWidth && ray->y < mapHeight)
+	{
+		int up;
+		int left;
+
+		up = 0;
+		left = 0;
+		if (ray->up == 1)
+			up = 1;
+		if (ray->left == 1)
+			left = 1;
+		if (Map[(int)ray->x - left][(int)ray->y - up] == 1)
+			return (1);
+		else
+			return (0);
+	}
+	return (-1);
+}
+
+int check_hit_loop(t_ray *ray)
+{
+	int	hit;
+
+	hit = check_hit(ray);
+	while (!hit)
+	{
+		ray->x += ray->stepX;
+		ray->x += ray->stepY;
+		hit = check_hit(ray);
+	}
+	return (hit);
+}
+
+int	find_h_intersection(t_ray *ray, t_mlx *mlx)
+{
+	if (ray->up != 0)
+	{
+		if (ray->up == 1)
+		{
+			ray->y = floor(mlx->posY);
+			ray->stepY = -1;
+		}
+		else
+		{
+			ray->y = floor(mlx->posY) + 1;
+			ray->stepY = 1;
+		}
+		if (ray->left != 0)
+		{
+			ray->x = mlx->posX + (mlx->posY - ray->y) / tan(ray->rayAngle);
+			ray->stepX = (1 / tan(ray->rayAngle)) * ray->up;
+		}
+		else
+		{
+			ray->x = mlx->posX;
+			ray->stepX = 0;
+		}
+		return (check_hit_loop(ray));
+	}
+	return (-1);
+}
+int	find_v_intersection(t_ray *ray, t_mlx *mlx)
+{
+	if (ray->left != 0)
+	{
+		if (ray->left == 1)
+		{
+			ray->x = floor(mlx->posX);
+			ray->stepX = -1;
+		}
+		else
+		{
+			ray->x = floor(mlx->posX) + 1;
+			ray->stepX = 1;
+		}
+		ray->y = mlx->posY + (mlx->posX - ray->x) * tan(ray->rayAngle);
+		ray->stepY = tan(ray->rayAngle) * ray->left;
+		return (check_hit_loop(ray));
+	}
+	return (-1);
+}
+
+void find_nearest_intersection(t_ray *ray, t_mlx *mlx)
+{
+	double distH;
+	double distV;
+	if (ray->hX != -1)
+	{
+		distH = ABS(mlx->posX - ray->hX) + ABS(mlx->posY - ray->hY);
+		distV = ABS(mlx->posX - ray->vY) + ABS(mlx->posY - ray->vY);
+		if (ray->vX == -1 || distH < distV)
+		{
+			ray->x = ray->hX;
+			ray->y = ray->hY;
+		}
+	}
+}
+
+void find_intersection(t_ray *ray, t_mlx *mlx)
+{
+		getRayDir(ray->rayAngle, ray->up, ray->left);
+		if (find_h_intersection(&ray, mlx) == 1)
+		{
+			ray->hX = ray->x;
+			ray->hY = ray->y;
+		}
+		if (find_v_intersection(&ray, mlx) == 1)
+		{
+			ray->vX = ray->x;
+			ray->vY = ray->y;
+		}
+		find_nearest_intersection(&ray, mlx);
+		printf("(%f;%f)\n", ray->x, ray->y);
+		ray->rayAngle -= mlx->stepRad;
+}
+
 void	render(t_mlx *mlx)
 {
-	t_ray ray;
-	int x = 0;
-	int hit = 0;
-	int up = 0;
-	int left = 0;
+	t_ray	ray;
+	int		x;
+	//init_ray
 	ray.rayAngle = mlx->angle + (mlx->fov / 2); //check if > 2 * M_PI
-	while(x < 1)
-	{
-		getRayDir(ray.rayAngle, &up, &left);
-		//horizontal intersection
-		if (up != 0)
-		{
-			if (up == 1)
-			{
-				ray.hY = floor(mlx->posY);
-				ray.stepY = -1;
-			}
-			else
-			{
-				ray.hY = floor(mlx->posY) + 1;
-				ray.stepY = 1;
-			}
+	int x;
 
-			if (left != 0)
-			{
-				ray.hX = mlx->posX + (mlx->posY - ray.hY) / tan(ray.rayAngle);
-				ray.stepX = (1 / tan(ray.rayAngle)) * up;
-			}
-			else
-			{
-				ray.hX = mlx->posX;
-				ray.stepX = 0;
-			}
-			//TO_DO: hit = check hit
-			while (!hit)
-			{
-				ray.hX += ray.stepX;
-				ray.hY += ray.stepY;
-				//TO_DO: hit = check hit
-			}
-		}
-		hit = 0;
-		//TO_DO: vertical intersection
-		if (left != 0)
-		{
-			if (left == 1)
-			{
-				ray.vX = floor(mlx->posX);
-				ray.stepX = -1;
-			}
-			else
-			{
-				ray.vX = floor(mlx->posX) + 1;
-				ray.stepX = 1;
-			}
-			ray.vY = mlx->posY + (mlx->posX - ray.vX) * tan(ray.rayAngle);
-			ray.stepY = tan(ray.rayAngle) * left;
-			//TO_DO: hit = check hit
-			while (!hit)
-			{
-				ray.vX += ray.stepX;
-				ray.vY += ray.stepY;
-				//TO_DO: hit = check hit
-			}
-		}
-		//TO_DO: find nearest intersection
-		printf("(%f;%f), sX: %f, sY: %f, alpha: %f, %d\n", ray.hX, ray.hY, ray.stepX, ray.stepY, ray.rayAngle, up);
+	x = 0;
+	while(x < screenW)
+	{
+		find_intersection(&ray, mlx)
+		
 		x++;
-	}
+	{
 }
 
 int	main()
@@ -135,7 +201,7 @@ int	main()
 	mlx.posX = 1.5, mlx.posY = 1.5;  //x and y start position
 	mlx.angle = 15 * M_PI / 180;
 	mlx.fov = 60 * M_PI / 180;
-	mlx.stepRad = (double)screenW / mlx.fov;
+	mlx.stepRad = mlx.fov / screenW;
 
 	//init mlx
 	mlx.mlx = mlx_init();
