1e8a0144cfa7283777bba23154932d00cdd3ded3
Valentin Beaufays <beaufaysvalentin@gmail.com>
2021-04-03 02:34:07 +0200

    error/ parser.h render.h

--- includes/render.h
+++ includes/render.h
@@ -3,24 +3,15 @@
 
 # include "struct.h"
 
-# define screenW 20
-# define screenH 20
-# define mapWidth 13
-# define mapHeight 6
-# define SIZE 64
-
-double  deg2rad(double deg);
-double  rad2deg(double rad);
-double  round_rad(double rad);
-void    getRayDir(double angle, int *up, int *left);
-int	    check_h_hit(t_ray *ray, t_cub3d *data);
-int	    check_v_hit(t_ray *ray, t_cub3d *data);
-int     check_hit_loop(t_ray *ray, t_cub3d *data, int (*c_h)(t_ray *ray, t_cub3d *data));
-int     find_h_intersect(t_ray *ray, t_cub3d *data);
-int	    find_v_intersection(t_ray *ray, t_cub3d *data);
-void    find_nearest_intersection(t_ray *ray, t_cub3d *data);
-void    find_intersection(t_ray *ray, t_cub3d *data);
-void    print_column(t_ray *ray, t_cub3d *data, int x);
-int     rgb_to_int(int r, int g, int b);
+double	deg2rad(double deg);
+double	rad2deg(double rad);
+double	round_rad(double rad);
+int		check_h_hit(t_ray *ray, t_cub3d *data);
+int		check_v_hit(t_ray *ray, t_cub3d *data);
+int		check_hit_loop(t_ray *ray, t_cub3d *data,
+						int (*c_h)(t_ray *ray, t_cub3d *data));
+void	find_intersection(t_ray *ray, t_cub3d *data);
+void	print_column(t_ray *ray, t_cub3d *data, int x);
+int		rgb_to_int(int r, int g, int b);
 
 #endif
