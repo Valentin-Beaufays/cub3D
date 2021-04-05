499256be18f4f9542aa5cf07ee652a446b788ae9
Valentin Beaufays <beaufaysvalentin@gmail.com>
2021-04-05 19:00:44 +0200

    movement is working

--- game/game_loop.c
+++ game/game_loop.c
@@ -21,25 +21,69 @@ int key_hook(int key, t_cub3d *data)
 	double	step_x;
 	double	step_y;
 
-	find_intersection(&data->ray, data);
-	dist_x = ft_abs(data->ray.i.x - data->pos.x);
-	dist_y = ft_abs(data->ray.i.y - data->pos.y);
+
 	if (key == UP)
 	{
+		find_intersection(&data->ray, data);
+		dist_x = ft_abs(data->ray.i.x - data->pos.x);
+		dist_y = ft_abs(data->ray.i.y - data->pos.y);
 		if (data->ray.dir == 1)
 		{
-			step_y = sin(data->angle) * 0.2;
+			step_y = sin(data->angle) * 0.1;
+			if (ft_abs(dist_y) <= ft_abs(step_y))
+				step_y = dist_y - 0.05 * data->ray.up;
 			step_x = step_y / tan(data->angle);
+			if (ft_abs(dist_x) <= ft_abs(step_x))
+				step_x = dist_x - 0.05 * data->ray.left;
 		}
 		else
 		{
-			step_x = cos(data->angle) * 0.2;
+			step_x = cos(data->angle) * 0.1;
+			if (ft_abs(dist_x) <= ft_abs(step_x))
+				step_x = dist_x - 0.05 * data->ray.left;
 			step_y = step_x * tan(data->angle);
+			if (ft_abs(dist_y) <= ft_abs(step_y))
+				step_y = dist_y - 0.05 * data->ray.up;
 		}
 		move_x = data->pos.x + step_x;
 		move_y = data->pos.y - step_y;
-		printf("pos:(%f;%f), new:(%f;%f), wall: (%f;%f) dist_x: %f, dist_y: %f, ste_x: %f, step_y: %f\n", data->pos.x, data->pos.y, move_x, move_y, data->ray.i.x, data->ray.i.y, dist_x, dist_y, step_x, step_y);
-		printf("angle: %f°\n", rad2deg(data->angle));
+		if (data->map.map[(int)move_y][(int)move_x] != 1)
+		{
+			data->pos.x = move_x;
+			data->pos.y = move_y;
+		}
+	}
+	if (key == DOWN)
+	{
+		data->ray.angle += M_PI;
+		find_intersection(&data->ray, data);
+		dist_x = ft_abs(data->ray.i.x - data->pos.x);
+		dist_y = ft_abs(data->ray.i.y - data->pos.y);
+		if (data->ray.dir == 1)
+		{
+			step_y = sin(data->angle) * 0.1;
+			if (ft_abs(dist_y) <= ft_abs(step_y))
+				step_y = dist_y - 0.05 * data->ray.up;
+			step_x = step_y / tan(data->angle);
+			if (ft_abs(dist_x) <= ft_abs(step_x))
+				step_x = dist_x - 0.05 * data->ray.left;
+		}
+		else
+		{
+			step_x = cos(data->angle) * 0.1;
+			if (ft_abs(dist_x) <= ft_abs(step_x))
+				step_x = dist_x - 0.05 * data->ray.left;
+			step_y = step_x * tan(data->angle);
+			if (ft_abs(dist_y) <= ft_abs(step_y))
+				step_y = dist_y - 0.05 * data->ray.up;
+		}
+		move_x = data->pos.x - step_x;
+		move_y = data->pos.y + step_y;
+		if (data->map.map[(int)move_y][(int)move_x] != 1)
+		{
+			data->pos.x = move_x;
+			data->pos.y = move_y;
+		}
 	}
 	
     render(data);
