1dc7e292fe445ddc2292c0fadda3f8c8234c2815
Valentin Beaufays <beaufaysvalentin@gmail.com>
2021-04-10 03:29:50 +0200

    rename var

--- render/sprite.c
+++ render/sprite.c
@@ -7,46 +7,46 @@
 int	out_of_bound_sprite_col(t_sprite *s, t_cub3d *d, int dir)
 {
 	double	tmp_angle;
-	int		pos;
+	int		col;
 
-	pos = 0;
+	col = 0;
 	if (dir == -1)
 	{
 		tmp_angle = d->angle_buf[0];
 		while (is_greater_angle(s->angle, tmp_angle))
 		{
 			tmp_angle = round_rad(tmp_angle + d->step_rad);
-			pos--;
+			col--;
 		}
 	}
 	else if (dir == 1)
 	{
 		tmp_angle = d->angle_buf[(int)d->def.x - 1];
-		pos = d->def.x - 1;
+		col = d->def.x - 1;
 		while (is_greater_angle(tmp_angle, s->angle))
 		{
 			tmp_angle = round_rad(tmp_angle - d->step_rad);
-			pos++;
+			col++;
 		}
 	}
-	return (pos);
+	return (col);
 }
 
 double	find_sprite_col(t_sprite *s, t_cub3d *d)
 {
-	int pos;
+	int col;
 
 	if (is_greater_angle(s->angle, d->angle_buf[0]))
-		pos = out_of_bound_sprite_col(s, d, -1);
+		col = out_of_bound_sprite_col(s, d, -1);
 	else if (is_greater_angle(d->angle_buf[(int)d->def.x - 1], s->angle))
-		pos = out_of_bound_sprite_col(s, d, 1);
+		col = out_of_bound_sprite_col(s, d, 1);
 	else
 	{
-		pos = 0;
-		while (s->angle < d->angle_buf[pos])
-			pos++;
+		col = 0;
+		while (s->angle < d->angle_buf[col])
+			col++;
 	}
-	return (pos);
+	return (col);
 }
 
 void	sprite_found(t_cub3d *data, double x, double y)
