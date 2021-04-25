0ebc0484d6bdc6485fa4b792a4e38e2675916d22
vbeaufay <beaufaysvalentin@gmail.com>
2021-04-25 16:43:42 +0200

    norme okay

--- srcs/parser/res_parser.c
+++ srcs/parser/res_parser.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   res_parser.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbeaufay <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/25 14:19:19 by vbeaufay          #+#    #+#             */
+/*   Updated: 2021/04/25 14:19:41 by vbeaufay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "struct.h"
 #include "error.h"
@@ -11,32 +22,39 @@ static int	get_num(int *num, char *line)
 	return (1);
 }
 
-void	parse_resolution(t_temp *temp)
+static int	fill_res(char *line, t_temp *temp)
 {
-	char	*line;
-
-	line = temp->trim + 1;
 	if (!(*line == ' '))
-		free_tmp_err("invalid R line", temp, 3);
+		return (0);
 	while (*line == ' ')
 		line++;
 	if (!get_num(&temp->x, line))
-		free_tmp_err("invalid R line", temp, 3);
+		return (0);
 	while (ft_isdigit(*line))
 		line++;
 	if (!(*line == ' '))
-		free_tmp_err("invalid R line", temp, 3);
+		return (0);
 	while (*line == ' ')
 		line++;
 	if (!get_num(&temp->y, line))
-		free_tmp_err("invalid R line", temp, 3);
+		return (0);
 	while (ft_isdigit(*line))
 		line++;
 	while (*line)
 	{
 		if (!(*line == ' '))
-			free_tmp_err("invalid R line", temp, 3);
+			return (0);
 		line++;
 	}
+	return (1);
+}
+
+void	parse_resolution(t_temp *temp)
+{
+	char	*line;
+
+	line = temp->trim + 1;
+	if (!fill_res(line, temp))
+		free_tmp_err("invalid R line", temp, 3);
 	temp->count++;
 }
