e48afd2abc2ac797d26505c81da90bffb806308c
Noah Addino <naddino@MacBook-Pro-de-Noah.local>
2021-05-04 14:47:44 +0200

    up

--- libft/srcs/get_next_line/ft_get_next_line.h
+++ libft/srcs/get_next_line/ft_get_next_line.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   ft_get_next_line.h                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: naddino <naddino@student.42.fr>            +#+  +:+       +#+        */
+/*   By: naddino <naddino@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/12 14:16:13 by naddino           #+#    #+#             */
-/*   Updated: 2020/09/12 14:31:02 by naddino          ###   ########.fr       */
+/*   Updated: 2021/05/04 14:47:32 by naddino          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,10 @@ int			    get_next_line(int fd, char **line);
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 64
+# endif
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX  _SC_OPEN_MAX
 # endif
 
 #endif
